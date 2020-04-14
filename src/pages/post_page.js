87b527c25bd4964c096a6b49eabd121ad610e32a
mefa <mfbyrktr@gmail.com>
2020-04-14 10:48:56 +0300

    post page filterfunctionchanged

--- src/pages/post_page.js
+++ src/pages/post_page.js
@@ -4,18 +4,21 @@ import Posts from '../blog-posts.json'
 import ReactMarkdown from "react-markdown"
 
 const PostPage = (props) => {
+    /* LOGIC */
     const calledPath = props.match.params.id
-    console.log(calledPath)
+    /* console.log(calledPath) */
 
-    const calledPost =  Posts.filter((post => {
-        console.log(post.id)
+    const calledPost =  Posts.find((post => {
+        /* console.log(post.id) */
         return post.id===Number(calledPath)
     }))
     console.log(calledPost)
-    return calledPost[0]?(
+
+    /* VIEW */
+    return calledPost?(
       <div>
-        <h1>{calledPost[0].title}</h1> 
-        <ReactMarkdown source={calledPost[0].content} escapeHtml={false} />
+        <h1>{calledPost.title}</h1> 
+        <ReactMarkdown source={calledPost.content} escapeHtml={false} />
       </div>
     ):
     <div>404</div>
