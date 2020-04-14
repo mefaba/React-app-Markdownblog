ea69c83da587779484f95c4c061c6c4525776887
mefa <mfbyrktr@gmail.com>
2020-04-14 13:27:29 +0300

    gihub link added

--- src/components/navbar/navbar.unit.js
+++ src/components/navbar/navbar.unit.js
@@ -4,13 +4,14 @@ import Nav from 'react-bootstrap/Nav'
 import {Link} from 'react-router-dom'
 
 const NavbarUnit = () => {
-
+    
     return (
         <div>
             <Navbar bg="warning" variant="light">
                 <Nav className="mr-auto">
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
                     <Nav.Link as={Link} to="/admin">Sign In</Nav.Link>
+                    <Nav.Link href="https://github.com/mefaba/ReactApp-MarkdownBlog">Github</Nav.Link>
                 </Nav>
             </Navbar>
         </div>
